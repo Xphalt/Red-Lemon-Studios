5fafde8dc847f202fb72bf7a475a62d96149685b
Thinkwanaut <39705721+Thinkwanaut@users.noreply.github.com>
2021-02-09 16:15:59 +0000

    + Added Projectile Shooting
    Bullet spawned with force in direction player is facing. Activated on player left click

---
+++ Assets/Game/Scripts/PlayerScript_Daniel.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * DANIEL BIBBY
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript_Daniel : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public GameObject GunPos;
+
+    public float ShootForce;
+    public float TargetDistance;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(BulletPrefab, GunPos.transform);
+            newBullet.GetComponent<Rigidbody>().AddForce((Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward*TargetDistance) - GunPos.transform.position).normalized * ShootForce);
+            newBullet.transform.SetParent(null);
+            Destroy(newBullet, 2);
+        }
+    }
+}
