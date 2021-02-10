a60a27ab4354cce33ee5d24a215678f16000ed47
VoidWolf6 <78429550+VoidWolf6@users.noreply.github.com>
2021-02-10 16:57:02 +0000

    + Basic pathfinding

--- Assets/Scenes/Zack/Enemy_Script.cs
+++ Assets/Scenes/Zack/Enemy_Script.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class Enemy_Script : MonoBehaviour
 {
+<<<<<<< Updated upstream
     // Start is called before the first frame update
     void Start()
+=======
+    public int HP;
+    private int damage;
+    private float moveSpeed;
+
+    public GameObject target;
+
+    void Start()
+    {
+        HP = 100;
+        damage = 10;
+        moveSpeed = 2.2f;
+    }
+    void Update()
+>>>>>>> Stashed changes
     {
         
     }
