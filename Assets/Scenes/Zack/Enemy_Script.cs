c3852573fdf421403853e8191d129b07ee3bc7e8
VoidWolf6 <78429550+VoidWolf6@users.noreply.github.com>
2021-02-10 16:08:15 +0000

    +Added motion and pathfinding

--- Assets/Scenes/Zack/Enemy_Script.cs
+++ Assets/Scenes/Zack/Enemy_Script.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Enemy_Script : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int HP = 100;
+    private int damage = 10;
+    private float moveSpeed = 2.2f;
 
-    // Update is called once per frame
+    public GameObject target;
+   
     void Update()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player");
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+
+        if (HP == 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player_Weapon")
+        {
+            HP -= damage;
+        }
+        else if (collision.gameObject.tag == "Player")
+        {
+            //collision.gameObject.GetComponent<Player_Script>().HP -= damage;
+        }
+
+
     }
 }
