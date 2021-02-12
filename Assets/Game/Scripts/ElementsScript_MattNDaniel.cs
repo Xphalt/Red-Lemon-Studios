5b8758e09a6ebd45ac31dc72b9a979e824501511
ArcadeWoof <mati@odkryta.pl>
2021-02-12 14:19:56 +0100

    +Added BulletElements
    Co-Authored-By: Thinkwanaut <39705721+Thinkwanaut@users.noreply.github.com>

---
+++ Assets/Game/Scripts/ElementsScript_MattNDaniel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElementsScript_MattNDaniel : MonoBehaviour
+{
+    public enum Elements { Fire, Water, Air, Earth, ElementsSize };
+
+    public Text ElementsText;
+
+    public Elements m_CurElement;
+
+    private void Start()
+    {
+        m_CurElement = Elements.Fire;
+    }
+
+    void Update()
+    {
+        ChangeElement();
+    }
+
+    void UpdateElementText()
+    {
+        ElementsText.text = m_CurElement.ToString();
+
+        switch (m_CurElement)
+        {
+            case Elements.Fire:
+                ElementsText.color = Color.red;
+                break;
+            case Elements.Water:
+                ElementsText.color = Color.blue;
+                break;
+            case Elements.Air:
+                ElementsText.color = Color.white;
+                break;
+            case Elements.Earth:
+                ElementsText.color = Color.yellow;
+                break;
+        }
+    }
+
+    void ChangeElement()
+    {
+        m_CurElement += Mathf.FloorToInt(Input.mouseScrollDelta.y);
+
+        if (m_CurElement >= Elements.ElementsSize)
+        {
+            m_CurElement -= Elements.ElementsSize;
+        }
+        else if (m_CurElement < 0)
+        {
+            m_CurElement += (int)Elements.ElementsSize;
+        }
+
+        UpdateElementText();
+    }
+}
--- Assets/Game/Scripts/PlayerScript_Daniel.cs
+++ Assets/Game/Scripts/PlayerScript_Daniel.cs
@@ -9,20 +9,46 @@ using UnityEngine;
 
 public class PlayerScript_Daniel : MonoBehaviour
 {
-    public GameObject BulletPrefab;
+    public GameObject BulletFire;
+    public GameObject BulletWater;
+    public GameObject BulletEarth;
+    public GameObject BulletAir;
     public GameObject GunPos;
 
     public float ShootForce;
     public float TargetDistance;
 
+    private GameObject ChosenBullet;
+
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject newBullet = Instantiate(BulletPrefab, GunPos.transform);
+            CheckElement();
+            GameObject newBullet = Instantiate(ChosenBullet, GunPos.transform);
             newBullet.GetComponent<Rigidbody>().AddForce((Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward*TargetDistance) - GunPos.transform.position).normalized * ShootForce);
             newBullet.transform.SetParent(null);
             Destroy(newBullet, 2);
+        }
+    }
+
+    void CheckElement()
+    {
+        switch(GetComponent<ElementsScript_MattNDaniel>().m_CurElement)
+        {
+            case ElementsScript_MattNDaniel.Elements.Fire:
+                ChosenBullet = BulletFire;
+                break;
+            case ElementsScript_MattNDaniel.Elements.Water:
+                ChosenBullet = BulletWater;
+                break;
+            case ElementsScript_MattNDaniel.Elements.Air:
+                ChosenBullet = BulletAir;
+                break;
+            case ElementsScript_MattNDaniel.Elements.Earth:
+                ChosenBullet = BulletEarth;
+                break;
         }
     }
 }
