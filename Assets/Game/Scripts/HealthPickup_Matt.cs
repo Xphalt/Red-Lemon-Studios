41837b42ee74fafcd91fb1bd6acfd54c05d494e3
ArcadeWoof <mati@odkryta.pl>
2021-02-12 14:32:44 +0100

    +Added Player Stats and Ammo

---
+++ Assets/Game/Scripts/HealthPickup_Matt.cs
@@ -0,0 +1,20 @@
+﻿/*
+    Mateusz Szymanski
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup_Matt : MonoBehaviour
+{
+    public float RestoreValue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<PlayerAttributes_Matt>().AddHealth(RestoreValue);
+            gameObject.SetActive(false);
+        }
+    }
+}
