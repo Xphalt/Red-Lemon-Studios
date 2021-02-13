bb6d91b3468f16b3cc4f585553a2f73879ba45e9
Linden Woodford <41804445+Xphalt@users.noreply.github.com>
2021-02-13 14:46:35 +0000

    + Added Crystal Ball Tool with Dash
    Me and @Thinkwanaut have added the crystal ball tool that the player is able to collect and use for a small dash. This dash has a cooldown before it can be used again.

--- Assets/Game/Scripts/Player/PlayerScript_Daniel.cs
+++ Assets/Game/Scripts/Player/PlayerScript_Daniel.cs
@@ -9,20 +9,46 @@ using UnityEngine;
 
 public class PlayerScript_Daniel : MonoBehaviour
 {
+    public enum Tools
+    {
+        noTool,
+        crystalBall
+    }
+
+
     public GameObject BulletFire;
     public GameObject BulletWater;
     public GameObject BulletEarth;
     public GameObject BulletAir;
     public GameObject GunPos;
+    private Rigidbody rb;
+    private CharacterController characterController;
 
     public float ShootForce;
     public float TargetDistance;
 
     private GameObject ChosenBullet;
 
+    public Tools Tool;
+    public float DashSpeed;
+
+    private Vector3 dashDist;
+    private float dashTimer = 0;
+    public float dashDuration;
+    private bool isDashing;
+
+    private float toolTimer = 0;
+    public float toolDuration;
+    private bool isToolAvailable;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
             CheckElement();
@@ -30,6 +56,35 @@ public class PlayerScript_Daniel : MonoBehaviour
             newBullet.GetComponent<Rigidbody>().AddForce((Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward*TargetDistance) - GunPos.transform.position).normalized * ShootForce);
             newBullet.transform.SetParent(null);
             Destroy(newBullet, 2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            UseTool();
+        }
+
+        if (isDashing)
+        {
+            dashTimer += Time.deltaTime;
+
+            characterController.Move(dashDist * Time.deltaTime);
+
+            if (dashTimer > dashDuration)
+            {
+                isDashing = false;
+                dashTimer = 0;
+            }
+        }
+
+        if (!isToolAvailable)
+        {
+            toolTimer += Time.deltaTime;
+
+            if (toolTimer > toolDuration)
+            {
+                isToolAvailable = true;
+                toolTimer = 0;
+            }
         }
     }
 
@@ -49,6 +104,35 @@ public class PlayerScript_Daniel : MonoBehaviour
             case ElementsScript_MattNDaniel.Elements.Earth:
                 ChosenBullet = BulletEarth;
                 break;
+        }
+    }
+
+    private void UseTool()
+    {
+        if (isToolAvailable)
+        {
+            if (Tool == Tools.crystalBall)
+            {
+                CrystalBallTool();
+            }
+
+            isToolAvailable = false;
+        }
+    }
+
+    private void CrystalBallTool()
+    {
+        isDashing = true;
+
+        dashDist = characterController.velocity.normalized * DashSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == ("Tool"))
+        {
+            Tool = Tools.crystalBall;
+            other.gameObject.SetActive(false);
         }
     }
 }
