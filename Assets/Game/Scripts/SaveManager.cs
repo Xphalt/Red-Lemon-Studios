695fafb6320d80a9e9885bac213a544d6f2eec96
VoidWolf6 <78429550+VoidWolf6@users.noreply.github.com>
2021-02-27 15:56:13 +0000

    Placed foundations

---
+++ Assets/Game/Scripts/SaveManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+//Zack Pilgrim
+
+public class SaveManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Serializable]
+    class Save
+    {
+
+    }
+
+    void Saving()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+        file.Close();
+    }
+
+    void Loading()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
+        Save save = (Save)bf.Deserialize(file); //Save class is created by us and stores all game data
+        file.Close();
+    }
+
+}
