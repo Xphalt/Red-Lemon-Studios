e3a8d9fc4c0b7062b625ed4363119e51d0a4417f
Thinkwanaut <39705721+Thinkwanaut@users.noreply.github.com>
2021-03-04 13:26:47 +0000

    + Added a basic SFX manager script
    Hopefully this helps, although changes will probably need to be made to accommodate 3D

---
+++ Assets/Game/Scripts/Managers/SFXScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SFXScript : MonoBehaviour
+{
+    public List<string> SFXNameList = new List<string>();
+    public List<AudioClip> AudioClipList = new List<AudioClip>();
+
+    private Dictionary<string, AudioClip> SFXDict = new Dictionary<string, AudioClip>();
+    public AudioSource SFXPrefab;
+
+    private AudioSource Music;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for (int s_index = 0; s_index < SFXNameList.Count; s_index++)
+        {
+            SFXDict.Add(SFXNameList[s_index], AudioClipList[s_index]);
+        }
+
+        Music = GetComponent<AudioSource>();
+    }
+
+    public void PlaySFX(string sfx, bool loop = false)
+    {
+        if (SFXDict.ContainsKey(sfx))
+        {
+            AudioSource newSFX = Instantiate(SFXPrefab).GetComponent<AudioSource>();
+            newSFX.PlayOneShot(SFXDict[sfx]);
+            newSFX.loop = loop;
+            if (!loop) Destroy(newSFX.gameObject, SFXDict[sfx].length);
+        }
+    }
+
+    public void PlaySFX(string sfx, string name, bool loop = false)
+    {
+        if (SFXDict.ContainsKey(sfx))
+        {
+            AudioSource newSFX = Instantiate(SFXPrefab).GetComponent<AudioSource>();
+            newSFX.PlayOneShot(SFXDict[sfx]);
+            newSFX.name = name;
+            newSFX.loop = loop;
+            if (!loop) Destroy(newSFX.gameObject, SFXDict[sfx].length);
+        }
+    }
+
+    public void PlaySFX(string sfx, float playTime, bool loop = false)
+    {
+        if (SFXDict.ContainsKey(sfx))
+        {
+            AudioSource newSFX = Instantiate(SFXPrefab).GetComponent<AudioSource>();
+            newSFX.PlayOneShot(SFXDict[sfx]);
+            newSFX.loop = loop;
+            if (!loop) Destroy(newSFX.gameObject, playTime);
+        }
+    }
+
+    public void StopSFX(string name)
+    {
+        GameObject SFXStopped = GameObject.Find(name);
+        if (SFXStopped) Destroy(SFXStopped);
+    }
+
+    public void PlayMusic()
+    {
+        Music.Play();
+    }
+
+    public void StopMusic()
+    {
+        Music.Stop();
+    }
+}
