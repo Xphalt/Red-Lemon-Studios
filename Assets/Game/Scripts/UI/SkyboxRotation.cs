3e7e5d97cad6c40a9ac16da72bd27baa3cd87ad0
Fate Stredňáková Show <64658099+ShowFate@users.noreply.github.com>
2021-03-16 21:41:42 +0000

    +Added script to change skybox colour
    It successfully changes between 2 colours but not more than that. I need to get a second pair of eyes of why the index is out of range.

--- Assets/Game/Scripts/UI/SkyboxRotation.cs
+++ Assets/Game/Scripts/UI/SkyboxRotation.cs
@@ -1,13 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SkyboxRotation : MonoBehaviour
 {
-    public float CameraSpeed;
+    public float CameraSpeed, ColourDuration;
+    public Material skyboxMateriel;
+    public List<Color> ColourList = new List<Color>();
+
+
+    private void Start()
+    {
+        for (int i = 0; i < ColourList.Count; i++)
+        {
+            ColourList.Add(ColourList[i]);
+        }
+    }
+
     void Update()
     {
         RenderSettings.skybox.SetFloat("_Rotation", Time.time * CameraSpeed);
         //Link to code: https://answers.unity.com/questions/651780/rotate-skybox-constantly.html
+
+
+        ChangeColour();
+
+    }
+
+    public void ChangeColour()
+    {
+
+        for (int i = 0; i < ColourList.Count; i++)
+        {
+
+            float lerp = Mathf.PingPong(Time.time, ColourDuration) / ColourDuration;
+            RenderSettings.skybox.SetColor("_Tint", Color.Lerp(ColourList[i], ColourList[i + 1], lerp));
+        }
     }
 }
