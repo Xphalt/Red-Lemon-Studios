b24130acd695f7d53342ab7e22c60b8c020decae
Fate Stredňáková Show <64658099+ShowFate@users.noreply.github.com>
2021-03-16 19:08:12 +0000

    + Added Trippy skybox

---
+++ Assets/Game/Scripts/UI/SkyboxRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotation : MonoBehaviour
+{
+    public float CameraSpeed;
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * CameraSpeed);
+        //Link to code: https://answers.unity.com/questions/651780/rotate-skybox-constantly.html
+    }
+}
