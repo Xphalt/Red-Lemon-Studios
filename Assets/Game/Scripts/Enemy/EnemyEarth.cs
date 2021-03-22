2a47e3f7700904be9a02d64910a914efbac1505a
Linden Woodford <41804445+Xphalt@users.noreply.github.com>
2021-03-22 20:14:56 +0000

    Revert "Revert "Merge branch 'origin/enemy-animations' into feature-production""
    This reverts commit ecce8c9d38cfdafda40678d1fda0fe251a91c40b.

--- Assets/Game/Scripts/Enemy/EnemyEarth.cs
+++ Assets/Game/Scripts/Enemy/EnemyEarth.cs
@@ -10,9 +10,6 @@ public class EnemyEarth : Enemy
     public float rollDuration = 1;
     public float rollDamage = 25;
 
-    public string chargeSound;
-    public string chargeEndSound;
-
 
     public override void Start()
     {
@@ -26,11 +23,12 @@ public class EnemyEarth : Enemy
     {
         base.Update();
 
-        if (attackTimer > rollDuration && rolling)
+        if (attackTimer > rollDuration)
         {
-            EndRoll();
+            rolling = false;
+            impactDamage = 0;
         }
-
+        Animate();
         if (!rolling)
         {
             if (CanSeePlayer()) movementState = EnemyStates.Chasing;
@@ -47,20 +45,30 @@ public class EnemyEarth : Enemy
             rolling = true;
             impactDamage = rollDamage;
             Shift((target.transform.position - transform.position).normalized * rollSpeed, rollDuration, 0);
-
-            sfxScript.PlaySFX3D(chargeSound, transform.position);
         }
 
         return false;
     }
 
-    private void EndRoll()
+    public override void Animate()
     {
-        rolling = false;
-        impactDamage = 0;
-        EndShift();
-
-        sfxScript.PlaySFX3D(chargeEndSound, transform.position);
+        base.Animate();
+        Animator MyAnim = gameObject.GetComponent<Animator>();
+        if (movementState == EnemyStates.Chasing)
+        {
+            MyAnim.SetBool("Motion", true);
+            MyAnim.SetBool("Attacking", true);
+        }
+        else if (movementState == EnemyStates.Idle)
+        {
+            MyAnim.SetBool("Motion", false);
+            MyAnim.SetBool("Attacking", false);
+        }
+        else if (movementState == EnemyStates.Patrolling)
+        {
+            MyAnim.SetBool("Motion", true);
+            MyAnim.SetBool("Attacking", false);
+        }
     }
 
     public override void TriggerStatusEffect(ElementHazardAilments effectStats)
@@ -68,32 +76,5 @@ public class EnemyEarth : Enemy
         base.TriggerStatusEffect(effectStats);
 
         Shift((effectStats.gameObject.transform.position - transform.position).normalized * effectStats.statusMagnitude, effectStats.statusEffectDuration, (1-knockbackRecovery), 1, true);
-    }
-
-    public override void OnCollisionEnter(Collision collision)
-    {
-        base.OnCollisionEnter(collision);
-        if (collision.gameObject.TryGetComponent(out CharacterBase collisionCharacter))
-        {
-            if (collisionCharacter.team != team) EndRoll();
-        }
-    }
-
-    public override void SaveEnemy(string saveID)
-    {
-        base.SaveEnemy(saveID);
-
-        saveID = "Enemy" + saveID;
-        SaveManager.UpdateSavedBool(saveID + "Rolling", rolling);
-
-        if (!rolling && shifting) EndShift();
-    }
-
-    public override void LoadEnemy(string loadID)
-    {
-        base.LoadEnemy(loadID);
-
-        loadID = "Enemy" + loadID;
-        rolling = SaveManager.GetBool(loadID + "Rolling");
     }
 }
