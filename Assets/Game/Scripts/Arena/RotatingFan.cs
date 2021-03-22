25c2cccebb736d6c9b66463af8700c3b6346dd40
MegaMadMorgan <78374511+MegaMadMorgan@users.noreply.github.com>
2021-03-22 07:28:02 +0000

    +added assets to air battle arena

---
+++ Assets/Game/Scripts/Arena/RotatingFan.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingFan : MonoBehaviour
+{
+    public float RotateSpeedX;
+    public float RotateSpeedY;
+    public float RotateSpeedZ;
+    void Update()
+    {
+            transform.Rotate(RotateSpeedX, RotateSpeedY, RotateSpeedZ);
+    }
+}
