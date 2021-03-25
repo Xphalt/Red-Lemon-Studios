6bdcc8cdcd4ee4664c3cc3f780c28a7a84f01b29
Thinkwanaut <39705721+Thinkwanaut@users.noreply.github.com>
2021-03-25 09:17:56 +0000

    # Separated moving platform from platform attaching

--- Assets/Game/Scripts/Arena/MovingPlatform.cs
+++ Assets/Game/Scripts/Arena/MovingPlatform.cs
@@ -10,8 +10,6 @@ public class MovingPlatform : MonoBehaviour
 
     private Vector3 moveVelocity;
 
-    private Dictionary<Transform, Transform> objectsPreviousParents = new Dictionary<Transform, Transform>();
-
     void Start()
     {
         moveVelocity = maxMovement.normalized * moveSpeed;
@@ -22,23 +20,5 @@ public class MovingPlatform : MonoBehaviour
     {
         transform.Translate(moveVelocity * Time.deltaTime); //Apologise for the translating but in this case is' easier to not have a rigidbody
         if ((transform.position - startPos).magnitude > maxMovement.magnitude) moveVelocity *= -1;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (!objectsPreviousParents.ContainsKey(collision.transform) && collision.transform.CompareTag("Player"))
-        {
-            objectsPreviousParents.Add(collision.transform, collision.transform.parent);
-            collision.transform.SetParent(transform);
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (objectsPreviousParents.ContainsKey(collision.transform) && collision.transform.CompareTag("Player"))
-        {
-            collision.transform.SetParent(objectsPreviousParents[collision.transform]);
-            objectsPreviousParents.Remove(collision.transform);
-        }
     }
 }
--- Assets/Game/Scripts/Arena/PlatformAttach.cs
+++ Assets/Game/Scripts/Arena/PlatformAttach.cs
@@ -6,19 +6,20 @@ public class PlatformAttach : MonoBehaviour
 {
     public Transform parentTransform;
     private Dictionary<Transform, Transform> objectsPreviousParents = new Dictionary<Transform, Transform>();
+    public bool playerOnly = true;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (!objectsPreviousParents.ContainsKey(collision.transform))
+        if (!objectsPreviousParents.ContainsKey(collision.transform) && (collision.transform.CompareTag("Player") || !playerOnly))
         {
             objectsPreviousParents.Add(collision.transform, collision.transform.parent);
-            collision.transform.SetParent(parentTransform);
+            collision.transform.SetParent(transform);
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (objectsPreviousParents.ContainsKey(collision.transform))
+        if (objectsPreviousParents.ContainsKey(collision.transform) && (collision.transform.CompareTag("Player") || !playerOnly))
         {
             collision.transform.SetParent(objectsPreviousParents[collision.transform]);
             objectsPreviousParents.Remove(collision.transform);
