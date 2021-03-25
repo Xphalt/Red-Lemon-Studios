d1ff059f176d78721ed2b0662363baaa945b46d5
Thinkwanaut <39705721+Thinkwanaut@users.noreply.github.com>
2021-03-25 15:37:06 +0000

    # Fixed air arena interactables and interactable prefabs

--- Assets/Game/Scripts/Arena/Interactable_Items.cs
+++ Assets/Game/Scripts/Arena/Interactable_Items.cs
@@ -28,15 +28,6 @@ public class Interactable_Items : MonoBehaviour
         }
         if (healthDrop == null) healthDrop = GetComponentInChildren<HealthPickup>(true);
 
-        foreach (PickUpBase ammo in ammoDrops)
-        {
-            ammo.transform.localPosition = Vector3.zero;
-            ammo.transform.localScale = Vector3.one;
-            ammo.transform.localRotation = Quaternion.identity;
-        }
-        healthDrop.transform.localPosition = Vector3.zero;
-        healthDrop.transform.localScale = Vector3.one;
-        healthDrop.transform.localRotation = Quaternion.identity;
         DropPercentage = Random.Range(0, 100);
         gameObject.SetActive(!destroyed);
     }
