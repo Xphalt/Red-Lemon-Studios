6780e7d29480bebca65bf757b92aac347e10e839
Fate Stredňáková Show <64658099+ShowFate@users.noreply.github.com>
2021-03-26 12:58:34 +0000

    +Changed UI HUD

--- Assets/Game/Scripts/UI/GUI_Manager.cs
+++ Assets/Game/Scripts/UI/GUI_Manager.cs
@@ -14,9 +14,8 @@ public class GUI_Manager : MonoBehaviour
     public Image healthBarFill, relicBarFill, ammoBarFill;
 
     //Elemental Group variables
-    public Image airImage, waterImage, fireImage, earthImage;
+    public Image equipedAmmoIcon;
     public Sprite airIcon, waterIcon, fireIcon, earthIcon;
-    public Sprite selectedAirIcon, selectedWaterIcon, selectedEarthIcon, selectedFireIcon;
     public Color lightPink = new Color(0.9f, 0.5f, 0.5f, 1);
     public Color lightBlue = new Color(0.09f, 0.2f, 0.9f, 1);
     public Color lightRed = new Color(1, 0.2f, 0.3f, 1);
@@ -88,23 +87,23 @@ public class GUI_Manager : MonoBehaviour
 
     public void HighlightSelectedAmmo()
     {
-        ResetElementalImages();
+       // ResetElementalImages();
         switch (player.elementChanger.m_CurElement)
         {
             case ElementTypes.Air:
-                airImage.sprite = selectedAirIcon;
+                equipedAmmoIcon.sprite = airIcon;
                 ammoBarFill.color = lightPink;
                 break;
             case ElementTypes.Water:
-                waterImage.sprite = selectedWaterIcon;
+                equipedAmmoIcon.sprite = waterIcon;
                 ammoBarFill.color = lightBlue;
                 break;
             case ElementTypes.Fire:
-                fireImage.sprite = selectedFireIcon;
+                equipedAmmoIcon.sprite = fireIcon;
                 ammoBarFill.color = lightRed;
                 break;
             case ElementTypes.Earth:
-                earthImage.sprite = selectedEarthIcon;
+                equipedAmmoIcon.sprite = earthIcon;
                 ammoBarFill.color = lightGreen;
                 break;
             default:
@@ -112,13 +111,13 @@ public class GUI_Manager : MonoBehaviour
         }
     }
 
-    private void ResetElementalImages()
-    {
-        airImage.sprite = airIcon;
-        waterImage.sprite = waterIcon;
-        fireImage.sprite = fireIcon;
-        earthImage.sprite = earthIcon;
-    }
+    //private void ResetElementalImages()
+    //{
+    //    airImage.sprite = airIcon;
+    //    waterImage.sprite = waterIcon;
+    //    fireImage.sprite = fireIcon;
+    //    earthImage.sprite = earthIcon;
+    //}
 
     /*__________________________________________________________
     Pause menu code
