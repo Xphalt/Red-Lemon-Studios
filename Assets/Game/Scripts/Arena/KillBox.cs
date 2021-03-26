3b55c8e6847d8a7964881325c9ba0767b2b09108
Thinkwanaut <39705721+Thinkwanaut@users.noreply.github.com>
2021-03-26 12:07:20 +0000

    + Added killbox to water level with closest respawn point functionality

--- Assets/Game/Scripts/Arena/KillBox.cs
+++ Assets/Game/Scripts/Arena/KillBox.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class KillBox : MonoBehaviour
 {
-    public Transform respawnPos;
+    public List<Transform> respawnPos;
     public int damage;
     public bool playerOnly;
 
-    private void OnTriggerEnter(Collider collision)
+    public bool AutoFind;
+
+    private void Awake()
     {
-        if (collision.TryGetComponent(out Player playerScript))
+        if (AutoFind)
+        {
+            respawnPos.Clear();
+            foreach (GameObject respawn in GameObject.FindGameObjectsWithTag("Respawn"))
+            {
+                if (respawn.transform.parent == transform) respawnPos.Add(respawn.transform);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Player playerScript))
         {
             playerScript.TakeDamage(damage);
-            playerScript.transform.position = respawnPos.position;
-            collision.attachedRigidbody.velocity = Vector3.zero;
+            Vector3 point = collision.GetContact(0).point;
+            Vector3 closestRespawn = respawnPos[0].position;
+            float closestDistance = (respawnPos[0].position - point).magnitude;
+            foreach (Transform nextRespawn in respawnPos)
+            {
+                if ((nextRespawn.position - point).magnitude < closestDistance)
+                {
+                    closestRespawn = nextRespawn.position;
+                    closestDistance = (nextRespawn.position - point).magnitude;
+                }
+            }
+            playerScript.transform.position = closestRespawn;
+            collision.rigidbody.velocity = Vector3.zero;
         }
-        else if (!playerOnly && collision.TryGetComponent(out Enemy enemyScript)) enemyScript.Die();
+        else if (!playerOnly && collision.gameObject.TryGetComponent(out Enemy enemyScript)) enemyScript.Die();
     }
 }
