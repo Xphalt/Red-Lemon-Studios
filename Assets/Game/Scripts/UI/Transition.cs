ce203e9587474f43fc4186ea1b854aaaa1d47844
Fate Stredňáková Show <64658099+ShowFate@users.noreply.github.com>
2021-04-10 19:32:56 +0100

    +scene transition

---
+++ Assets/Game/Scripts/UI/Transition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    public Animator transition;
+    public float delay;
+
+    public void LoadNextLevel()
+    {
+        //This loads the next scene by calling the next index of the build scenes
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("exitScene");
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
